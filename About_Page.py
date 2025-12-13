ccb079f24dc714f69754944f92220c949eb0e8bc
Ayesha Khalid <elricedwardtwerp@gmail.com>
2025-12-13 14:23:51 +0500

    About Page

---
+++ About_Page.py
@@ -0,0 +1,8 @@
+#about us page details
+
+print("\t\t\t\t\t\tAbout Us")
+print("After the tragic loss of seven year old Jibrael, there was a dire need for enhanced security around campus.")
+print("An innocent life may have been saved if security personnel location was known.We felt strongly for the berieved;")
+print("something had to be done to keep this from happening in the future.IntruWatch was designed with the safety of GIKIans in mind.")
+print("It keeps track of CCTV cameras, security guard locations and details of those entering and leaving campus to make GIKI safer for all it's inhabitants.")
+
